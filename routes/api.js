35e36d0107058be0677fbc11a9d563e07e18a8e8
Diana Navarro <navarroruiz.diana@gmail.com>
2020-06-30 04:52:18 -0500

    Include auth module

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const countryRouter = require('../modules/country/country.route');
+const authRouter = require('../modules/auth/auth.route');
 
 const apiRouter = express.Router();
 
 apiRouter.use('/country', countryRouter);
+apiRouter.use('/auth', authRouter);
 
 module.exports = apiRouter;
